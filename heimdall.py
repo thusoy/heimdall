99f61035950f6f7b2979f0709eb6a3580864a35d
Tarjei Husøy <tarjei@comoyo.com>
2013-03-05 17:06:41 +0100

    Added a class for notifying the user.
    Also fixed:
    - Used default dict methods for setting default values
    - General refactoring and cleaning up

--- heimdall.py
+++ heimdall.py
@@ -13,6 +13,7 @@ __version__ = '.'.join(str(num) for num in __version_info__)
 from datetime import datetime, timedelta
 from dateutil.parser import parse
 from os import path
+from psutil.error import NoSuchProcess
 from time import sleep
 from utils import readable_to_bytes, bytes_to_readable
 import logging
@@ -20,22 +21,23 @@ import psutil
 import yaml
 import sys
 
-notify = None
-if sys.platform == 'win32':
-    # Find a tool to make notifications on windows
-    pass
-elif sys.platform == 'darwin':
-    try:
-        # Mac, send notifications to message center
-        from pync import Notifier
-        notify = Notifier.notify
-    except ImportError:
-        pass
-
 config = None
 usage = []
 suspended = set()
+horn = None
 
+class Gjallarhorn(object):
+    """ When the horn is blown, all listeners will hear the sound."""
+
+    def __init__(self, *args):
+        self.funcs = list(args)
+
+    def add_listener(self, func):
+        self.funcs.append(func)
+
+    def blow(self, sound):
+        for func in self.funcs:
+            func(sound)
 
 def init():
     _load_config()
@@ -43,6 +45,7 @@ def init():
     _init_logging()
     _print_startup_info()
     _update_usage_log()
+    _init_horn()
     logging.debug('Config values:')
     for key, val in config.items():
         logging.debug('{} -> {}'.format(key, val))
@@ -54,7 +57,7 @@ def run():
         sleep(config['monitor_interval_in_s'])
 
 def limit(last_value):
-    limit = config['limit_in_bytes']
+    usage_limit = config['limit_in_bytes']
     past_usage = _get_window_usage()
     total = psutil.network_io_counters().bytes_sent
     logging.debug("Bytes sent: {}".format(bytes_to_readable(total)))
@@ -62,23 +65,25 @@ def limit(last_value):
     logging.debug("Delta: {}".format(usage_delta))
     pids = psutil.get_pid_list()
     for pid in pids:
-        process = psutil.Process(pid)
-        if process.name.lower() in config['watch_list']:
-            if past_usage >= limit:
-                if pid not in suspended:
-                    msg = 'Suspending process: {}'.format(process.name)
-                    logging.info(msg)
-                    Notifier.notify(msg)
-                    process.suspend()
-                    suspended.add(pid)
-            else:
-                for pid in suspended:
-                    proc = psutil.Process(pid)
-                    msg = 'Resuming process: {}'.format(proc.name)
-                    logging.info(msg)
-                    Notifier.notify(msg)
-                    proc.resume()
-                suspended.clear()
+        try:
+            process = psutil.Process(pid)
+            if process.name.lower() in config['watch_list']:
+                if past_usage >= usage_limit:
+                    if pid not in suspended:
+                        sound = 'Suspending process: {} ({})'.format(process.name, pid)
+                        horn.blow(sound)
+                        process.suspend()
+                        suspended.add(pid)
+                else:
+                    for pid in suspended:
+                        proc = psutil.Process(pid)
+                        sound = 'Resuming process: {}'.format(proc.name)
+                        horn.blow(sound)
+                        proc.resume()
+                    suspended.clear()
+        except NoSuchProcess:
+            logging.debug('Process was terminated externally: %s' % pid)
+            continue
     usage.append( (datetime.now(), usage_delta) )
     with open('usage_log.log', 'a') as usage_log:
         usage_log.write('{} {}\n'.format(datetime.now().isoformat(), usage_delta))
@@ -95,11 +100,11 @@ def _set_defaults():
         print('Quitting: You must specify a watch_list in config.yaml!')
         sys.exit(1)
     limit_in_bytes = readable_to_bytes(config.get('limit', '10 GB'))
-    config['limit_in_bytes'] = config.get('limit_in_bytes', limit_in_bytes)
-    config['log_file'] = config.get('log_file', 'log.log')
+    config.setdefault('limit_in_bytes', limit_in_bytes)
+    config.setdefault('log_file', 'log.log')
     config['watch_list'] = set([p.lower() for p in config.get('watch_list', [])])
-    config['monitor_interval_in_s'] = config.get('monitor_interval_in_s', 5)
-    config['window_size_in_h'] = config.get('window_size_in_h', 24)
+    config.setdefault('monitor_interval_in_s', 5)
+    config.setdefault('window_size_in_h', 24)
 
 def _init_logging():
     msg_format = '%(asctime)s %(levelname)-10s %(message)s'
@@ -121,17 +126,20 @@ def _get_window_usage():
 
     The first value should be among the very first in most cases, so no need
     to do binary search."""
-    window_usage = 0
-    limit_date = datetime.now() - timedelta(hours=config['window_size_in_h'])
+    limit_date = datetime.now() - timedelta(seconds=config['window_size_in_h'])
+    first_valid_index = -1
     for index, (date, val) in enumerate(usage):
         if date >= limit_date:
-            window_usage = sum(val for date, val in usage[index:])
+            first_valid_index = index
             break
+    window_usage = sum(val for date, val in usage[first_valid_index:])
+    del usage[:first_valid_index]
+    print('Window: ' + str(["%s: %s" % (d.strftime("%H:%M:%S"), t) for d, t in usage]))
     return window_usage
 
 def _update_usage_log():
     if path.exists('usage_log.log'):
-        limit_date = datetime.now() - timedelta(hours=config['window_size_in_h'])
+        limit_date = datetime.now() - timedelta(seconds=config['window_size_in_h'])
         with open('usage_log.log', 'r') as usage_log:
             for line in usage_log:
                 datestring, val = line.split()
@@ -140,9 +148,21 @@ def _update_usage_log():
                     usage.append( (date, int(val)) )
         with open('usage_log.log', 'w') as usage_log:
             for date, val in usage:
-                usage_log.write('{} {}'.format(date.isoformat(), val))
+                usage_log.write('{} {}\n'.format(date.isoformat(), val))
     else:
-        with open('usage_log.log', 'w'):
+        open('usage_log.log', 'w').close()
+
+def _init_horn():
+    global horn
+    horn = Gjallarhorn(logging.info)
+    if sys.platform == 'win32':
+        #TODO: Find a tool to make notifications on windows
+        pass
+    elif sys.platform == 'darwin': #Mac
+        try:
+            from pync import Notifier
+            horn.add_listener(Notifier.notify)
+        except ImportError:
             pass
 
 if __name__ == '__main__':
@@ -156,4 +176,4 @@ if __name__ == '__main__':
             process.resume()
         logging.exception(e.message)
         info = sys.exc_info()
-        raise info[0], info[1], info[2]
+        raise (info[0], info[1], info[2])
